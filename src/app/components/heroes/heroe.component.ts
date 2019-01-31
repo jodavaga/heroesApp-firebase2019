62c812386cd3abaeb52a801a6db0602d87680080
Jose Daniel Vasquez Garcia <josedaniel.vasquez@globant.com>
2019-01-30 21:14:01 -0500

    Heroes service is updating info, and getting params by url

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Heroe } from '../../interfaces/heroe.interface';
 
@@ -20,20 +20,47 @@ export class HeroeComponent implements OnInit {
     casa: 'Marvel'
   }
 
+  nuevo: boolean = false;
+  id:string = "";
+
   constructor( private _heroesService:HeroesService,
-               private router:Router
-              ) { }
+               private router:Router,
+               private activatedRoute:ActivatedRoute
+              ) { 
+  
+  this.activatedRoute.params.subscribe( params => {
+    console.log(params)
+    if (params.id == "nuevo" ){
+      this.nuevo = true;
+      
+    }else {
+      this.nuevo = false;
+      this.id = params.id
+    }
+  })
+
+  }
 
   ngOnInit() {
   }
 
   guardar(){
 
-    this._heroesService.nuevoHeroe( this.heroe )
-        .subscribe( data => {
-          console.log( data )
-          this.router.navigate(['/heroe', data.name ])
-        })
+    if(this.nuevo){
+      this._heroesService.nuevoHeroe( this.heroe )
+          .subscribe( data => {
+            console.log( data )
+            this.router.navigate(['/heroe', data.name ])
+  
+          }, error => console.log(error));
+
+    }else {
+      this._heroesService.actualizarHeroe( this.heroe, this.id )
+            .subscribe( heroe => {
+              console.log(heroe)
+            })
+    }
+
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -3,6 +3,7 @@ import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 import { Heroe } from '../interfaces/heroe.interface';
+import { ɵKeyEventsPlugin } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Heroe } from '../interfaces/heroe.interface';
 export class HeroesService {
 
   heroesUrl: string = "https://heroesapp-udemy-941bc.firebaseio.com/heroes.json";
+  heroeUrl:string = "https://heroesapp-udemy-941bc.firebaseio.com/heroes";
 
   constructor( private http:Http ) { }
 
@@ -24,7 +26,18 @@ export class HeroesService {
     return this.http.post( this.heroesUrl, body, { headers } )
     .pipe( map( resp => {
       return resp.json()
-    }));
-        
+    }));     
   }
+
+  actualizarHeroe( heroe:Heroe, key:string ){
+     let body = JSON.stringify( heroe );
+
+     let url = `${ this.heroeUrl }/${ key }.json`
+
+     return this.http.put( url, body )
+                .pipe( map( resp => {
+                    return resp.json()
+                  }));
+  }
+
 }
